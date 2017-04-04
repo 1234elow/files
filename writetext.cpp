357cab99aa392b3e39c1ec287a06c862892b50c3
1234elow <samuellowe90@hotmail.com>
2017-04-03 20:35:04 -0400

    abc.txt

--- writetext.cpp
+++ writetext.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main( )
 {
    //create some data
+   ofstream out;
+   out.open("abc.txt");
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
  
    cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
-
+   out<<a<<" "<<b<<" "<<c;
    cout<<endl<<endl;
  
    return 0;
